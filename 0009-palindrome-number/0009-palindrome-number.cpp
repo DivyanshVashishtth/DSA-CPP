749f4ff2ab818c5ac99da5170465ecb6cbd2b6b1
Divyansh Vashishtth <divyanshvashishtth123@gmail.com>
2026-08-09 12:56:23 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.13%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        int temp = x;
+        long int rev = 0;
+        while (x > 0){
+            rev = (rev*10) + x%10;
+            x = x/10;
+        }
+        if(rev == temp){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
